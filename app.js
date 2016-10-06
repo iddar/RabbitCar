d1f7cb6137bbbf6eead42583e3880498f6601919
iddar <iddar@dbug.mx>
2016-10-06 05:58:53 -0500

    set camera and new function for generate world

--- app.js
+++ app.js
@@ -4,6 +4,33 @@
 const width = 800
 const height = 600
 
+const LANES = 4
+
+function getBar (lanes) {
+  let lines = lanes
+  if (lines < 3) lines = 3
+  let base_up = [1, 2, 3]
+  let base_down = [5, 6, 7]
+
+  let center = new Array(lines - 2).fill(4)
+  return base_up.concat(center, base_down)
+}
+
+function drawBar (game, group, bar, x, y) {
+  for (let position = y, i = 0; position < (y + (32 * bar.length)); position += 32, i++) {
+    let groundBlock = game.add.sprite(x, position, 'ground', `ground_${bar[i]}`)
+
+    if (bar[i] < 3 || bar[i] > 5) {
+      game.physics.enable(groundBlock, Phaser.Physics.ARCADE)
+      groundBlock.body.collideWorldBounds = true
+      groundBlock.body.checkCollision.right = false
+      groundBlock.body.checkCollision.left = false
+      groundBlock.body.immovable = true
+      group.add(groundBlock)
+    }
+  }
+}
+
 class RabbitCar {
   constructor (game) {
     this.game = game
@@ -11,66 +38,60 @@ class RabbitCar {
 
   preload () {
     this.game.load.spritesheet('car', '/car.png', 32, 32, 8)
-    this.game.load.spritesheet('ground', '/ground.png', 32, 32, 8)
+    // this.game.load.spritesheet('ground', '/ground.png', 32, 32, 8)
+    game.load.atlasJSONHash('ground', '/ground.png', '/ground.json')
   }
 
   create () {
     // world style
     this.game.stage.backgroundColor = 0x4488cc
     this.game.physics.startSystem(Phaser.Physics.ARCADE)
+    this.game.world.setBounds(0, 0, 32 * 200, height)
     // load background
-    // this.tiles = this.game.add.sprite(0, 0, 'ground', 3)
-    // this.background = this.add.tileSprite(0, 0, 32, 32, 'ground', 3)
-    // this.background.autoScroll(-30, 0)
 
-    this.border = this.game.add.sprite(0, 200 - 32, 'ground', 0)
-    // this.game.physics.enable(this.border, Phaser.Physics.ARCADE)
-    this.game.physics.arcade.enable(this.border)
-    this.border.body.collideWorldBounds = true
-    this.border.body.bounce.set(1)
-
-    // for (let x = 0; x < this.game.width; x += 32) {
-    //   let groundBlock = this.game.add.sprite(x, 200 - 32, 'ground', 0)
-    //   this.game.physics.enable(groundBlock, Phaser.Physics.ARCADE)
-    //   groundBlock.body.collideWorldBounds = true
-    //   groundBlock.body.immovable = true
-    //   this.border.add(groundBlock)
-    // }
-
-    this.ground = this.game.add.group()
-    for (let y = 200; y < (200 + (3 * 32)); y += 32) {
-      for (let x = 0; x < this.game.width; x += 32) {
-        let groundBlock = this.game.add.sprite(x, y, 'ground', 3)
-        this.ground.add(groundBlock)
-      }
+    this.border = game.add.group()
+    for (let x = 0; x < (32 * 200); x += 32) {
+      drawBar(this.game, this.border, getBar(10), x, 100)
     }
 
     // load assets
     this.car = this.game.add.sprite(300, 200, 'car')
     this.walk = this.car.animations.add('walk')
     this.car.animations.play('walk', 10, true)
+    this.game.camera.follow(this.car)
     this.game.physics.arcade.enable(this.car)
-    // Make player collide with world boundaries so he doesn't leave the stage
-    // this.game.physics.enable(this.car, Phaser.Physics.ARCADE)
     this.car.body.collideWorldBounds = true
-    this.car.body.bounce.set(1)
+    this.game.physics.enable(this.car)
+
+    // Make player collide with world boundaries so he doesn't leave the stage
 
     // Events
     this.cursors = this.game.input.keyboard.createCursorKeys()
   }
 
   update () {
-    this.game.physics.arcade.collide(this.border, this.car)
+    let velocity = 8
+    if (this.game.physics.arcade.collide(this.border, this.car)) {
+      velocity = 2
+    } else {
+      velocity = 8
+    }
+
     if (this.cursors.left.isDown) {
-      this.car.x -= 8
-    } else if (this.cursors.right.isDown) {
-      this.car.x += 8
-    } else if (this.cursors.up.isDown) {
+      this.car.x -= velocity
+      this.game.camera.follow(this.car, Phaser.Camera.FOLLOW_PLATFORMER)
+    }
+    if (this.cursors.right.isDown) {
+      this.car.x += velocity
+    }
+    if (this.cursors.up.isDown) {
       this.car.y -= 8
-    } else if (this.cursors.down.isDown) {
+    }
+    if (this.cursors.down.isDown) {
       this.car.y += 8
     }
   }
+
 }
 
 var game = new Phaser.Game(width, height, Phaser.AUTO, 'game')
